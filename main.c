f7371566c31576f922f30ca3e03ccd71a3e3b870
hanlei666-python <3404477082@qq.com>
2022-06-04 21:03:57 +0800

    version 2.0

--- main.c
+++ main.c
@@ -1,16 +1,12 @@
-//#include <iostream>
-// extern "C"
-// {
-    #include <libavformat/avformat.h>
-    #include <libavutil/log.h>
-    #include <libavformat/avio.h>
-    #include <libavutil/file.h>
-    #include <libavutil/error.h>
-    #include <openssl/evp.h>
-    #include <openssl/applink.c>
-    #include <openssl/pem.h>
-    #include <openssl/rsa.h>
-// }
+#include <libavformat/avformat.h>
+#include <libavutil/log.h>
+#include <libavformat/avio.h>
+#include <libavutil/file.h>
+#include <libavutil/error.h>
+#include <openssl/evp.h>
+#include <openssl/applink.c>
+#include <openssl/pem.h>
+#include <openssl/rsa.h>
 #include <stdio.h>
 int main(int argc, char* argv[]) {
     printf("Hello, world!\n");
