c4ab3274f4ebfe5fb5bf5f5a32656950aa4f27d9
hanlei666-python <3404477082@qq.com>
2022-06-04 20:52:10 +0800

    initial example file

---
+++ main.c
@@ -0,0 +1,18 @@
+//#include <iostream>
+// extern "C"
+// {
+    #include <libavformat/avformat.h>
+    #include <libavutil/log.h>
+    #include <libavformat/avio.h>
+    #include <libavutil/file.h>
+    #include <libavutil/error.h>
+    #include <openssl/evp.h>
+    #include <openssl/applink.c>
+    #include <openssl/pem.h>
+    #include <openssl/rsa.h>
+// }
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+    printf("Hello, world!\n");
+    return 0;
+}
